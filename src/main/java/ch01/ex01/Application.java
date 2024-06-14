d196831c3816118b495ef96a82c5b7b0b2f778ac
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-06-14 09:41:42 +0900

    Menu, Button 클릭 이벤트 분리

--- src/main/java/ch01/ex01/Application.java
+++ src/main/java/ch01/ex01/Application.java
@@ -10,27 +10,36 @@ public class Application {
     private Menu menu2 = new Menu("menu2");
     private Button button1 = new Button("button1");
 
-    private ScreenUi currentMenu = null;
+    private ScreenUi currentScreen = null;
 
     public Application2() {
-        menu1.setOnClickListener(this);
-        menu2.setOnClickListener(this);
-        button1.setOnClickListener(this);
+        menu1.setOnClickListener(menuListener);
+        menu2.setOnClickListener(menuListener);
+        button1.setOnClickListener(buttonListener);
     }
 
-    public void clicked(Component eventSource) {
-        String sourceId = eventSource.getId();
+    private OnClickListener menuListener = new OnclickListener() {
+        public void clicked(Component eventSource) {
+            String sourceId = eventSource.getId();
 
-        if (sourceId.equals("menu1")) {
-            currentMenu = new Menu1ScreenUi();
-            currentMenu.show();
-        } else if (sourceId.equals("menu1")) {
-            currentMenu = new Menu2ScreenUi();
-            currentMenu.show();
-        } else if (sourceId.equals("button1")) {
-            if (currentMenu == null)
-                return;
-            currentMenu.handleButton1Click();
+            if (sourceId.equals("menu1")) {
+                currentScreen = new Menu1ScreenUi();
+            } else if (sourceId.equals("menu1")) {
+                currentScreen = new Menu2ScreenUi();
+            }
+            currentScreen.show();
         }
-    }
+    };
+
+    private OnClickListener buttonListener = new OnclickListener() {
+        public void clicked(Component eventSource) {
+            if (currentScreen == null)
+                return;
+
+            String sourceId = eventSource.getId();
+            if (sourceId.equals("button1")) {
+                currentScreen.handleButton1Click();
+            }
+        }
+    };
 }
