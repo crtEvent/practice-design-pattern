2336b62f8d6f7eb67a7e1573e7c98259a27013fc
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-06-18 01:01:37 +0900

    전략패턴: 기존 코드

---
+++ src/main/java/ch07/strategy/Item.java
@@ -0,0 +1,14 @@
+package ch07.strategy;
+
+public class Item {
+    private int price;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public boolean isFresh() {
+        return true;
+    }
+}
+
