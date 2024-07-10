f446dbcc44307c10d9b861446667b9d0f684df87
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-07-10 15:20:10 +0900

    정적 팩토리 메서드: 서버 생성 메서드

---
+++ src/main/kotlin/kdpbp2e/creational/factorymethod/StaticFactoryMethod.kt
@@ -0,0 +1,18 @@
+package kdpbp2e.creational.factorymethod
+
+fun main() {
+    Server.withPort(8080)
+}
+
+class Server private constructor(port: Long) {
+
+    init {
+        println("Server started on port $port")
+    }
+
+    companion object {
+        fun withPort(port: Long): Server {
+            return Server(port)
+        }
+    }
+}
