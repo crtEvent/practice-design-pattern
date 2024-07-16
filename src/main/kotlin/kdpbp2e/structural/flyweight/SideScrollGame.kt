5eedd33d54df7e4325f8b9c0dc792f37f3b78e5b
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-07-17 03:54:44 +0900

    경량 패턴: 문제 해결

--- src/main/kotlin/kdpbp2e/structural/flyweight/SideScrollGame.kt
+++ src/main/kotlin/kdpbp2e/structural/flyweight/SideScrollGame.kt
@@ -9,9 +9,11 @@ enum class Direction {
 /*
  * [문제]
  * 이미지의 용량이 크다면 TanzanianSnail 객체를 많이 만들기 부담스럽다
+ *
+ * [해결]
+ * 무거운 부분을 따로 빼서 제한적으로 생성하고 가벼운 경량 객체들이 무거운 객체를 공유하도록 한다
  */
-class TanzanianSnail {
-    val directionFacing = Direction.LEFT
+object SnailSprites {
     val sprites = List(8) { i ->
         File(when(i) {
             0 -> "snail-left.png"   // 왼쪽을 향하고 있는 이미지
@@ -20,6 +22,11 @@ class TanzanianSnail {
             else -> "snail-move-right-${4-i}.png"        // 오른쪽으로 움직이는 이미지
         })
     }
+}
+
+class TanzanianSnail() {
+    private val directionFacing = Direction.LEFT
+    private val sprites = SnailSprites.sprites
 
     fun getCurrentSprite(): File {
         return when (directionFacing) {
