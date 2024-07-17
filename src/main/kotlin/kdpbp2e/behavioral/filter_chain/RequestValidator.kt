8f7912ef7193777ce97f8d458cca196140439d07
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-07-17 22:31:51 +0900

    책임 사슬 패턴: 함수로 코드를 더 간결하게! (나는 별로..)

--- src/main/kotlin/kdpbp2e/behavioral/filter_chain/RequestValidator.kt
+++ src/main/kotlin/kdpbp2e/behavioral/filter_chain/RequestValidator.kt
@@ -2,6 +2,9 @@ package kdpbp2e.behavioral.filter_chain
 
 /* 책임 사슬 패턴
  * 복잡한 로직을 여러 개의 작은 단계로 쪼개는 방법!
+ *
+ * 코틀린에선 함수를 사용해서 코드를 더 간결하게 할 수 있음(그렇게 많이 간결해 진것도 아닌데?)
+ * 라이브러리나 프레임 워크 처럼 다른 개발자가 기능을 확장할 가능성이 높다면 함수보다는 인터페이스를 사용하는게 더 명시적임
  */
 fun main() {
     val req = Request(
@@ -9,15 +12,15 @@ fun main() {
         "질문 있어요!"
     )
 
-    val chain = BasicValidationHandler(
-        KnownEmailHandler(
-            JuniorDeveloperFilterHandler(
-                AnswerHandler()
+    val chain = basicValidation(
+        authentication(
+            juniorValidation(
+                finalResponse()
             )
         )
     )
 
-    val res = chain.handle(req)
+    val res = chain(req)
 
     println(res)
 }
@@ -35,42 +38,35 @@ data class Request(val email:String, val question: String) {
 
 data class Response(val answer: String)
 
-interface Handler {
-    fun handle(request: Request): Response
-}
+typealias Handler = (request:Request) -> Response
 
-class BasicValidationHandler(private val next: Handler): Handler {
-    override fun handle(request: Request): Response {
+val basicValidation = fun(next: Handler) =
+    fun(request: Request): Response {
         if (request.email.isEmpty() || request.question.isEmpty()) {
             throw IllegalArgumentException()
         }
-
-        return next.handle(request)
+        return next(request)
     }
-}
 
-class KnownEmailHandler(private val next: Handler): Handler {
-    override fun handle(request: Request): Response {
+val authentication = fun(next: Handler) =
+    fun(request: Request): Response {
         if (!request.isKnownEmail()) {
             throw IllegalArgumentException()
         }
 
-        return next.handle(request)
+        return next(request)
     }
-}
 
-class JuniorDeveloperFilterHandler(private val next: Handler): Handler {
-    override fun handle(request: Request): Response {
+val juniorValidation = fun(next: Handler) =
+    fun(request: Request): Response {
         if (!request.isFromJuniorDeveloper()) {
             throw IllegalArgumentException()
         }
 
-        return next.handle(request)
+        return next(request)
     }
-}
 
-class AnswerHandler(): Handler {
-    override fun handle(request: Request): Response {
+val finalResponse = fun() =
+    fun(request: Request): Response {
         return Response("최종 결과!")
     }
-}
