db460aece5dc61792577024b3c1f3731a8a67291
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-07-17 22:23:35 +0900

    책임 사슬 패턴: 책임 사슬 패턴을 사용한 유효성 검증 로직

--- src/main/kotlin/kdpbp2e/behavioral/filter_chain/RequestValidator.kt
+++ src/main/kotlin/kdpbp2e/behavioral/filter_chain/RequestValidator.kt
@@ -1,30 +1,76 @@
 package kdpbp2e.behavioral.filter_chain
 
+/* 책임 사슬 패턴
+ * 복잡한 로직을 여러 개의 작은 단계로 쪼개는 방법!
+ */
+fun main() {
+    val req = Request(
+        "developer@company.com",
+        "질문 있어요!"
+    )
+
+    val chain = BasicValidationHandler(
+        KnownEmailHandler(
+            JuniorDeveloperFilterHandler(
+                AnswerHandler()
+            )
+        )
+    )
+
+    val res = chain.handle(req)
+
+    println(res)
+}
+
+
 data class Request(val email:String, val question: String) {
     fun isKnownEmail(): Boolean {
         return true
     }
 
     fun isFromJuniorDeveloper(): Boolean {
-        return false
+        return true
     }
 }
 
-fun handleRequest(r: Request) {
-    // Validate 유효성 검사
-    if (r.email.isEmpty() || r.question.isEmpty()) {
-        return
-    }
+data class Response(val answer: String)
 
-    // Authenticate 인증
-    if (r.isKnownEmail()) {
-        return
-    }
+interface Handler {
+    fun handle(request: Request): Response
+}
 
-    // Authorize 인가
-    if (r.isFromJuniorDeveloper()) {
-        return
-    }
+class BasicValidationHandler(private val next: Handler): Handler {
+    override fun handle(request: Request): Response {
+        if (request.email.isEmpty() || request.question.isEmpty()) {
+            throw IllegalArgumentException()
+        }
 
-    println("몰라요~")
+        return next.handle(request)
+    }
+}
+
+class KnownEmailHandler(private val next: Handler): Handler {
+    override fun handle(request: Request): Response {
+        if (!request.isKnownEmail()) {
+            throw IllegalArgumentException()
+        }
+
+        return next.handle(request)
+    }
+}
+
+class JuniorDeveloperFilterHandler(private val next: Handler): Handler {
+    override fun handle(request: Request): Response {
+        if (!request.isFromJuniorDeveloper()) {
+            throw IllegalArgumentException()
+        }
+
+        return next.handle(request)
+    }
+}
+
+class AnswerHandler(): Handler {
+    override fun handle(request: Request): Response {
+        return Response("최종 결과!")
+    }
 }
