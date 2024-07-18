3b9c11c28a16be42d4fe81f41681f8cb70b612ff
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-07-18 18:06:24 +0900

    템플릿 메소드: 코틀린 함수를 활용한 템플릿 메소드 패턴

--- src/main/kotlin/kdpbp2e/behavioral/template_method/MyDayRoutine.kt
+++ src/main/kotlin/kdpbp2e/behavioral/template_method/MyDayRoutine.kt
@@ -1,54 +1,36 @@
 package kdpbp2e.behavioral.template_method
 
 fun main() {
-    val mondaySchedule = MondaySchedule()
-    mondaySchedule.runSchedule()
+    runSchedule(
+        beforeLunch = { println("월요일 점심 전엔?") },
+        afterLunch = { println("월요일 점심 후엔?") },
+        bossHook = { println("매니저가 한마디!") },
+    )
 }
 
-abstract class DayRoutine {
-    abstract fun doBeforeLunch()
-    abstract fun doAfterLunch()
+fun runSchedule(
+    beforeLunch: () -> Unit,
+    afterLunch: () -> Unit,
+    bossHook: (() -> Unit)? = fun() { println("??")},
+) {
 
-    fun runSchedule() {
-        arriveToWork()
-        drinkCoffee()
-        doBeforeLunch()
-        goToLunch()
-        doAfterLunch()
-        goHome()
-    }
-
-    open fun bossHook() {
-        println("??")
-    }
-
-    private fun arriveToWork() {
+    fun arriveToWork() {
         println("사무실에 등장!")
     }
 
-    private fun drinkCoffee() {
-        println("커피 마심")
-    }
+    val drinkCoffee = { println("커피 마심") }
 
-    private fun goToLunch() {
-        println("점심 먹음")
-    }
+    fun goToLunch() = println("점심 먹음")
 
-    private fun goHome() {
+    val goHome = fun() {
         println("퇴근")
     }
-}
 
-class MondaySchedule : DayRoutine() {
-    override fun doBeforeLunch() {
-        println("월요일 점심 전엔?")
-    }
-
-    override fun doAfterLunch() {
-        println("월요일 점심 후엔?")
-    }
-
-    override fun bossHook() {
-        println("월요일???")
-    }
+    arriveToWork()
+    drinkCoffee()
+    beforeLunch()
+    goToLunch()
+    afterLunch()
+    bossHook?.let { it() }
+    goHome()
 }
