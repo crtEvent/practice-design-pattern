22f5f858b56e95196fa400234d5ddbfb77584b6b
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-07-31 00:40:09 +0900

    상태 패턴: 요구사항 추가

--- src/main/java/oodpdmm/ch07/state/VendingMachine.java
+++ src/main/java/oodpdmm/ch07/state/VendingMachine.java
@@ -9,10 +9,13 @@ package oodpdmm.ch07.state;
  * | 제품 선택 | 동전 없음이면     | 아무 동작 없음     | 동전 없음 유지          |
  * | 제품 선택 | 제품 선택 가능이면 | 제품 주고 잔액 감소 | 잔액 있으면 제품 선택 가능 |
  * |         |               |                 | 잔액 없으면 동전 없음     |
+ *
+ * [요구사항 추가]
+ * - 자판기에 제품이 없는 경우 동전을 넣으면 동전을 되돌려 준다가
  */
 
 public class VendingMachine {
-    private enum State { NO_COIN, SELECTABLE }
+    private enum State { NO_COIN, SELECTABLE, SOLD_OUT }
     private State state = State.NO_COIN;
     private int coin = 0;
 
@@ -24,12 +27,13 @@ public class VendingMachine {
             case SELECTABLE -> {
                 increaseCoin(coin);
             }
+            case SOLD_OUT -> returnCoin(); // 상태에 대한 요구사항이 추가될 때 마다 조건문 코드가 추가된다
         }
     }
 
     public void select(int productId) {
         switch (state) {
-            case NO_COIN -> {}
+            case NO_COIN, SOLD_OUT -> {}
             case SELECTABLE -> {
                 provideProduct(productId);
                 decreaseCoin();
@@ -53,6 +57,9 @@ public class VendingMachine {
     }
 
     private void provideProduct(int productId) {
+    }
+
+    private void returnCoin() {
     }
 
 
